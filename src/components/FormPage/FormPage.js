d50d7a42bacb5b0023364a379343431ef6376195
TomSoerr <tomix2004@gmail.com>
2023-02-23 18:27:13 +0100

    Add form page

--- src/components/FormPage/FormPage.js
+++ src/components/FormPage/FormPage.js
@@ -5,8 +5,18 @@ import Hero from "../Hero/Hero.js";
 const FormInput = (props) => {
   return (
     <label>
-      {props.name}
-      <input type="text" name={props.name.toLowerCase()} />
+      {props.name} *
+      <input
+        placeholder=" "
+        pattern={props.pattern}
+        required
+        type={props.type}
+        name={props.name.toLowerCase()}
+        onInvalid={(e) => e.target.setCustomValidity(props.msg)}
+        onInput={(e) => e.target.setCustomValidity("")}
+        title={props.msg}
+      />
+      <span></span>
     </label>
   );
 };
@@ -14,8 +24,16 @@ const FormInput = (props) => {
 const FormTextArea = (props) => {
   return (
     <label>
-      {props.name}
-      <textarea name={props.name.toLowerCase()}></textarea>
+      {props.name} *
+      <textarea
+        placeholder=" "
+        required
+        name={props.name.toLowerCase()}
+        onInvalid={(e) => e.target.setCustomValidity(props.msg)}
+        onInput={(e) => e.target.setCustomValidity("")}
+        title={props.msg}
+      ></textarea>
+      <span></span>
     </label>
   );
 };
@@ -30,22 +48,34 @@ const FormPage = (props) => {
       <Hero
         class="form"
         box={
-          <form
-            // action="https://webhook.site/1cf3fb6d-573b-4bd5-b7ea-4b97e4bed4a7"
-            method="get"
-            id="contact-form"
-          >
+          <form autoComplete="on" id="contact-form">
+            <p>Pflichtfelder sind mit einem Sternchen (*) gekennzeichnet.</p>
             {[
               {
                 name: "Name",
+                pattern: "[a-zA-Z]{2,}",
+                msg: "Bitte geben Sie einen gültigen Namen ohne Leerzeichen und Umlaute ein der mindestens zwei Buchstaben hat.",
+                type: "text",
               },
               {
                 name: "E-Mail",
+                pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$",
+                msg: "Bitte geben Sie eine E-Mail ohne Leerzeichen, Großbuchstaben und Umlaute ein.",
+                type: "email",
               },
             ].map((input, index) => (
-              <FormInput key={index} name={input.name} />
+              <FormInput
+                msg={input.msg}
+                key={index}
+                name={input.name}
+                pattern={input.pattern}
+                type={input.type}
+              />
             ))}
-            <FormTextArea name="Nachricht" />
+            <FormTextArea
+              msg="Bitte geben sie ein Nachricht ein"
+              name="Nachricht"
+            />
             <FormSubmitBtn text="Absenden" />
           </form>
         }
